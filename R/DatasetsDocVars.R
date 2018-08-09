ada656f4add600e23b40504ca9df55bf35e08cff
Your <you@example.com>
2018-08-09 10:45:34 -0500

    Upload .Rd files

---
+++ R/DatasetsDocVars.R
@@ -0,0 +1,87 @@
+#' stud2015vars
+#' @description Dataset describing the variables for stud2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"stud2015vars"
+
+#' stud2015_2vars
+#' @description Dataset describing the variables for stud2015_2
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"stud2015_2vars"
+
+#' sch2015vars
+#' @description Dataset describing the variables for sch2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"sch2015vars"
+
+#' cog2015vars
+#' @description Dataset describing the variables for cog2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"cog2015vars"
+
+#' tim2015vars
+#' @description Dataset describing the variables for tim2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"tim2015vars"
+
+#' tch2015vars
+#' @description Dataset describing the variables for tch2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"tch2015vars"
+
+#' lit2015vars
+#' @description Dataset describing the variables for lit2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"lit2015vars"
+
+#' cps2015vars
+#' @description Dataset describing the variables for cps2015
+#'
+#' @format A dataframe of 3 Variables
+#' \describe{
+#'  \item{Names}{Name of the Variable}
+#'  \item{Description}{Brief Description of the Variable}
+#'  \item{Type}{What Type of Varible is it? (Nominal, Ordinal, Numeric, Ratio)}
+#' }
+"cps2015vars"
