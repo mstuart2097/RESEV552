bb34792f188c3c32a5ddaab0a44f5c3e3d884f6e
Your <you@example.com>
2018-08-14 13:58:04 -0500

    Add Portioned Data

---
+++ R/DatasetsDoc.R
@@ -0,0 +1,41 @@
+#' @name Data
+#' @title PISA 2015 Datasets
+#' @description These are the datasets from the PISA 2015 Survey
+#'
+#' @format Dataframe with the Specific Variables for the Dataset
+#' A more detailed description can be found in the corresponding pdf file
+#' 
+"cog2015.part1"
+
+#' @rdname Data
+"cog2015.part2"
+
+#' @rdname Data
+"cog2015.part3"
+
+#' @rdname Data
+"cog2015.part4"
+
+#' @rdname Data
+"cps2015"
+
+#' @rdname Data
+"lit2015"
+
+#' @rdname Data
+"sch2015"
+
+#' @rdname Data
+"stud2015.part1"
+
+#' @rdname Data
+"stud2015.part2"
+
+#' @rdname Data
+"stud2015_2"
+
+#' @rdname Data
+"tch2015"
+
+#' @rdname Data
+"tim2015"
---
+++ R/DatasetsDocVars.R
@@ -0,0 +1,45 @@
+#' @name DataVars
+#' @title PISA 2015 Dataset Variables
+#' @description These are the Variable Description Datasets from the PISA 2015 Survey
+#'
+#' @format Dataframe with the Specific Variables for the Dataset
+#' \describe{
+#'  \item {Names}{Name of the Variable from the Dataset}
+#'  \item {Description}{Brief Description of Variable from the Dataset}
+#'  \item {Type}{Type of Variable}
+#' }
+#' 
+"cog2015.part1"
+
+#' @rdname Data
+"cog2015.part2"
+
+#' @rdname Data
+"cog2015.part3"
+
+#' @rdname Data
+"cog2015.part4"
+
+#' @rdname Data
+"cps2015"
+
+#' @rdname Data
+"lit2015"
+
+#' @rdname Data
+"sch2015"
+
+#' @rdname Data
+"stud2015.part1"
+
+#' @rdname Data
+"stud2015.part2"
+
+#' @rdname Data
+"stud2015_2"
+
+#' @rdname Data
+"tch2015"
+
+#' @rdname Data
+"tim2015"
